5036853705adcd79a83bdb6aacac8488fc1a00a6
minwoo0419 <ghdl464@naver.com>
2024-04-11 15:15:03 +0900

    [feat] Question Domain 구현

---
+++ src/main/java/com/telepigeon/server/domain/Question.java
@@ -0,0 +1,29 @@
+package com.telepigeon.server.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name="question")
+@NoArgsConstructor( access = AccessLevel.PROTECTED)
+public class Question {
+    @Id
+    @Column(name="room_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="content", nullable = false)
+    private String content;
+
+    @Column(name="created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(targetEntity = Profile.class, fetch = FetchType.LAZY)
+    @JoinColumn(name="profile_id", nullable = false)
+    private Profile profile;
+}
