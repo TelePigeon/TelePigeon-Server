6677b69c84398850d173ae2dab7e4e5467b6de78
minwoo0419 <ghdl464@naver.com>
2024-04-11 15:15:18 +0900

    [feat] Room Domain 구현

---
+++ src/main/java/com/telepigeon/server/domain/Room.java
@@ -0,0 +1,25 @@
+package com.telepigeon.server.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name="room")
+@NoArgsConstructor( access = AccessLevel.PROTECTED)
+public class Room {
+    @Id
+    @Column(name="room_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="code", nullable = false, unique = true)
+    private String code;
+
+    @Column(name="created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
