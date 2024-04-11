17409b94cd7fd0dc6690da49511196f00a33e163
minwoo0419 <ghdl464@naver.com>
2024-04-11 15:14:56 +0900

    [feat] Answer Domain 구현

---
+++ src/main/java/com/telepigeon/server/domain/Answer.java
@@ -0,0 +1,36 @@
+package com.telepigeon.server.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name="answer")
+@NoArgsConstructor( access = AccessLevel.PROTECTED)
+public class Answer {
+    @Id
+    @Column(name="answer_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="content", nullable = false)
+    private String content;
+
+    @Column(name="image")
+    private String image;
+
+    @Column(name="created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @OneToOne(targetEntity = Question.class, fetch = FetchType.LAZY)
+    @JoinColumn(name="question_id", nullable = false)
+    private Question question;
+
+    @ManyToOne(targetEntity = Profile.class, fetch = FetchType.LAZY)
+    @JoinColumn(name="profile_id", nullable = false)
+    private Profile profile;
+}
