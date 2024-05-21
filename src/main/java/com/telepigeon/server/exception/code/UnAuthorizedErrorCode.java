b4b214ceb77e70ee3b2412eac74958a8baf1d4f3
tkdwns414 <sangjune2000@naver.com>
2024-05-22 03:28:11 +0900

    [feat] #24 add comments in UnAuthorizedErrorCode

--- src/main/java/com/telepigeon/server/exception/code/UnAuthorizedErrorCode.java
+++ src/main/java/com/telepigeon/server/exception/code/UnAuthorizedErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum UnAuthorizedErrorCode implements DefaultErrorCode{
+    //external
     INVALID_KAKAO_TOKEN(HttpStatus.UNAUTHORIZED, "error", "카카오 토큰이 유효하지 않습니다."),
+    //internal
     INVALID_JWT(HttpStatus.UNAUTHORIZED, "error", "유효하지 않은 토큰입니다."),
     EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "error", "만료된 토큰입니다."),
     UNSUPPORTED_JWT(HttpStatus.UNAUTHORIZED, "error", "지원하지 않는 토큰입니다."),
