75ce821f32382cddd98b36cbae32a65e91722197
tkdwns414 <sangjune2000@naver.com>
2024-05-28 03:20:47 +0900

    [feat] #24 create Token and Service Helper

--- src/main/java/com/telepigeon/server/exception/code/NotFoundErrorCode.java
+++ src/main/java/com/telepigeon/server/exception/code/NotFoundErrorCode.java
@@ -14,6 +14,7 @@ public enum NotFoundErrorCode implements DefaultErrorCode{
     ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "error", "존재하지 않는 방입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "error", "존재하지 않는 사용자입니다."),
     PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "error", "존재하지 않는 프로필입니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "error", "존재하지 않는 리프레시 토큰입니다."),
     ;
 
     @JsonIgnore
---
+++ src/main/java/com/telepigeon/server/service/auth/TokenRetreiver.java
@@ -0,0 +1,19 @@
+package com.telepigeon.server.service.auth;
+
+import com.telepigeon.server.exception.NotFoundException;
+import com.telepigeon.server.exception.code.NotFoundErrorCode;
+import com.telepigeon.server.repository.TokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TokenRetreiver {
+
+    private final TokenRepository tokenRepository;
+
+    public Long findIdByRefreshToken(final String refreshToken){
+        return tokenRepository.findIdByRefreshToken(refreshToken)
+                .orElseThrow(() -> new NotFoundException(NotFoundErrorCode.REFRESH_TOKEN_NOT_FOUND));
+    }
+}
