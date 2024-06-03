23379575329728ad7c32f0c7c1b0f2ec8b069e6d
tkdwns414 <sangjune2000@naver.com>
2024-06-03 23:41:16 +0900

    [fix] #48 reduce DTO

--- src/main/java/com/telepigeon/server/dto/openAi/response/OpenAiResponseDto.java
+++ src/main/java/com/telepigeon/server/dto/openAi/response/OpenAiResponseDto.java
@@ -1,8 +1,19 @@
 package com.telepigeon.server.dto.openAi.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.telepigeon.server.dto.openAi.MessageDto;
+
 import java.util.List;
 
 public record OpenAiResponseDto (
     List<ChoiceDto> choices
-){
+) {
+    public record ChoiceDto(
+            @JsonProperty(value = "finish_reason")
+            String finishReason,
+            String index,
+            MessageDto message,
+            Object logprobs
+    ) {
+    }
 }
--- src/main/java/com/telepigeon/server/oauth/dto/KakaoUserDto.java
+++ src/main/java/com/telepigeon/server/oauth/dto/KakaoUserDto.java
@@ -1,9 +1,14 @@
 package com.telepigeon.server.oauth.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoUserDto(Long id, KakaoAccount kakaoAccount){
+    public record KakaoAccount (@JsonProperty("profile") KakaoUserProfile kakaoUserProfile, String email){
+        public record KakaoUserProfile (String nickname){
 
+        }
+    }
 }
