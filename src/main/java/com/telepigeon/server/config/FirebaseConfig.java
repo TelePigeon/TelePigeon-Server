b8e1ef0b1eab3954e97379cd8fb3814172dd199a
minwoo0419 <ghdl464@naver.com>
2024-06-06 00:28:33 +0900

    [fix] #67 file 생성 확인 테스트 추가2

--- src/main/java/com/telepigeon/server/config/FirebaseConfig.java
+++ src/main/java/com/telepigeon/server/config/FirebaseConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
